09ee4b9e503c1593c38560e047cfc408e118fc1e
TW0DAYS <ansdPgml0425@gmail.com>
2023-10-31 23:07:17 +0900

    Update Mover.js

--- Mover.js
+++ Mover.js
@@ -72,3 +72,62 @@ if (mouseIsPressed) {
   let wind = createVector(0.1, 0);
   mover.applyForce(wind);
 }
+
+class Mover {
+  constructor() {
+    this.mass = 1;
+
+    this.position = createVector(width / 2, 30);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+  }
+
+  applyForce(force) {
+
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+
+
+    this.acceleration.mult(0);
+  }
+
+  show() {
+    stroke(0);
+    fill(175);
+    circle(this.position.x, this.position.y, this.mass * 16);
+
+  }
+
+  checkEdges() {
+    if (this.position.x > width) {
+      this.position.x = width;
+      this.velocity.x *= -1;
+    } else if (this.position.x < 0) {
+      this.velocity.x *= -1;
+      this.position.x = 0;
+    }
+
+
+    if (this.position.y > height) {
+      this.velocity.y *= -1;
+      this.position.y = height;
+
+    }
+  }
+}
+
+let moverA = new Mover();
+let moverB = new Mover();
+
+constructor() {
+  this.mass = 1;
+  this.position = createVector(width / 2, 30);
+
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
+}
